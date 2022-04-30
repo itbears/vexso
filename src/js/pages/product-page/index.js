c8da024cf3805e83cacaec5634cb79f719754134
Redheady <iam@redheady.ru>
2022-04-30 21:34:45 +0300

    Адаптив для product-page и правка хлебных крошек

--- src/js/pages/product-page/index.js
+++ src/js/pages/product-page/index.js
@@ -1,24 +1,35 @@
 import Swiper from 'swiper/bundle';
 
 const productThumbs = new Swiper(".js-product-thumbs", {
-    direction: "vertical",
-    spaceBetween: 15,
-    slidesPerView: 6,
-    watchSlidesProgress: true,
+    spaceBetween: 20,
+    slidesPerView: 4,
+    freeMode: true,
+    breakpoints: {
+        450: {
+            spaceBetween: 20,
+            slidesPerView: 6,
+        },
+        700: {
+            spaceBetween: 20,
+            slidesPerView: 4,
+        },
+        1050: {
+            direction: "vertical",
+            spaceBetween: 15,
+            slidesPerView: 6,
+            watchSlidesProgress: true,
+        }
+    }
 });
 
 const productSlider = new Swiper(".js-product-slider", {
     spaceBetween: 30,
-    effect: "fade",
+    effect: 'fade',
     navigation: {
         nextEl: ".js-product-slider .swiper-button-next",
         prevEl: ".js-product-slider .swiper-button-prev",
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
     thumbs: {
         swiper: productThumbs,
-    },
+    }
 });
