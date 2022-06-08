981d34460463fb61b833d1546fc2fbb2e185bae4
Redheady <iam@redheady.ru>
2022-06-08 13:08:28 +0300

    Update shop

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,15 @@ import './pages/main/index';
 import "./pages/product-page/index";
 import "./pages/cart/index";
 
+const filterLabels = document.querySelectorAll(".wpfLiLabel")
+
+filterLabels.forEach((label, i) => {
+    label.addEventListener("click", () => {
+        label.classList.add("active")
+        filterLabels.forEach(l => {
+            l.classList.remove("active")
+        })
+    })
+})
+
 
