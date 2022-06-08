6a2d93e57b1bfb26fb4f86de4877e4ab358b7c90
Redheady <iam@redheady.ru>
2022-06-08 14:45:58 +0300

    check

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,4 @@ import './header';
 import './pages/main/index';
 import "./pages/product-page/index";
 
-const filterLabels = document.querySelectorAll(".wpfLiLabel")
-
-filterLabels.forEach((label, i) => {
-    label.addEventListener("click", () => {
-        label.classList.add("active")
-        filterLabels.forEach(l => {
-            l.classList.remove("active")
-        })
-    })
-})
-
 
