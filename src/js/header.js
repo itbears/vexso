fcc137e3e26df160af1e9143a7971276061ad70b
Redheady <iam@redheady.ru>
2022-06-08 13:16:25 +0300

    Revert "Merge branch 'denis' of https://github.com/itbears/vexso into denis"
    This reverts commit 1787a42f65843c20f6dd7be7c653d0424a040f41, reversing
    changes made to b7abdf2968f1ed3db2429b74ab5aff22bcbda09a.

--- src/js/header.js
+++ src/js/header.js
@@ -2,15 +2,10 @@
 const menuBurger = document.querySelector(".js-menu-burger")
 const menu = document.querySelector(".js-menu")
 const menuBack = document.querySelector(".js-menu-back")
-const body = document.querySelector("body")
-const menuBtns = document.querySelectorAll(".js-menu-item-btn")
-const menuSubMenus = document.querySelectorAll(".js-menu-item-menu")
 
-// Menu
 menuBurger.addEventListener("click", () => {
     menu.classList.toggle("active")
     menuBack.classList.toggle("active")
-    body.classList.toggle("scroll-off")
     setTimeout(() => {
         menuBack.classList.toggle("on")
     }, 400)
@@ -18,28 +13,15 @@ menuBurger.addEventListener("click", () => {
 
 menuBack.addEventListener("mouseover", () => {
     if (menuBack.classList.contains("on")) {
-        body.click()
         menu.classList.add("exit")
         setTimeout(() => {
             menu.classList.toggle("active")
             menuBack.classList.toggle("active")
             menuBack.classList.toggle("on")
             menu.classList.toggle("exit")
-            body.classList.toggle("scroll-off")
         }, 500)
     }
 })
-
-menuBtns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        menuSubMenus[i].classList.toggle("active")
-        setTimeout(() => {
-            menuSubMenus[i].classList.toggle("animate")
-        }, 50)
-    })
-})
-
-
 
 // Scroll Header
 const header = document.querySelector(".js-header")
